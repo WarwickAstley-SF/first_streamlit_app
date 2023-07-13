fd3277203f84770f02965dc33a4a0c627b5bbd95
WarwickAstley-SF <139411393+WarwickAstley-SF@users.noreply.github.com>
2023-07-13 21:32:43 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,13 +47,27 @@ except URLError as e:
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-
 streamlit.header("The fruit load list contrains:")
-streamlit.dataframe(my_data_rows)
+#snowflake-related functions
+def get_fruit_load_list();
+      with my_cnx.cursor() as my_cur:
+           my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
+           return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_list()
+      streamlit.dataframe(my_data_rows)
+      
+
+
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contrains:")
+#streamlit.dataframe(my_data_rows)
 
 #add text input
 text_input = streamlit.text_input("What fruit would you like to add?", "Enter selection here")
